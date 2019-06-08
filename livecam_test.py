3f84638816d60ad83d0982314065cc511f892df9
Peace <santisuk.ji@tnis.com>
2019-06-08 12:39:06 +0700

    example of livestream code from rtsp app in mobile

---
+++ livecam_test.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+import os
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+
+#vcap = cv2.VideoCapture("rtsp://192.168.1.2:5554/camera", cv2.CAP_FFMPEG)
+vcap = cv2.VideoCapture("rtsp://10.59.56.248:5554/camera", cv2.CAP_FFMPEG)
+while(1):
+    ret, frame = vcap.read()
+    if ret == False:
+        print("Frame is empty")
+        break
+    else:
+        cv2.imshow('VIDEO', frame)
+        cv2.waitKey(1)
+
+#REF : https://medium.com/beesightsoft/opencv-python-connect-to-android-camera-via-rstp-9eb78e2903d5
