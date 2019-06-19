096e7d24a05aa089652abd76fe5869109a45b0a7
Peace <santisuk.ji@tnis.com>
2019-06-19 22:22:18 +0700

    add card matching

--- project.py
+++ project.py
@@ -68,12 +68,12 @@ dim = (640, 480)
 lmain = Label(imageFrame)
 lmain.grid(row=0, column=0)
 
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-import os
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
-# #vcap = cv2.VideoCapture("rtsp://192.168.1.2:5554/camera", cv2.CAP_FFMPEG)
-cap = cv2.VideoCapture("rtsp://10.184.111.218:5554/camera", cv2.CAP_FFMPEG)
+# import os
+# os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+# # #vcap = cv2.VideoCapture("rtsp://192.168.1.2:5554/camera", cv2.CAP_FFMPEG)
+# cap = cv2.VideoCapture("rtsp://10.184.111.218:5554/camera", cv2.CAP_FFMPEG)
 
 def setIcon(icon_no = 1):
     if icon_no == 2:#Green
@@ -104,12 +104,120 @@ def countScore(party_no):
     c.grid(row=11,column=0)
 
 tmp = 0
-def detect_card():
-    global tmp
-    if tmp > 100:
-        tmp = 0
-    tmp = tmp+1
-    return (tmp%15) == 0
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	return rect
+
+
+def four_point_transform(image, pts):
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	return warped
+
+template_bgr = cv2.imread('card_border_black.jpg')
+template_gray = cv2.cvtColor( template_bgr, cv2.COLOR_BGR2GRAY )
+h,w = template_gray.shape[:2]
+
+# detector = cv2.xfeatures2d.SIFT_create()
+detector = cv2.ORB_create()
+template_kps,template_descs = detector.detectAndCompute( template_gray, None )
+FLANN_INDEX_KDTREE = 0
+FLANN_INDEX_LSH = 6
+# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+index_params = dict(algorithm = FLANN_INDEX_LSH,
+                        table_number = 6,       # 12
+                        key_size = 12,          # 20
+                        multi_probe_level = 1)  # 2
+search_params = dict(checks=50)
+# Initiate the matcher
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+def detect_card(cam_bgr):
+    print(cam_bgr.shape)
+    cam_gray = cv2.cvtColor( cam_bgr, cv2.COLOR_BGR2GRAY )
+    # Find sets of keypoints and descriptors
+    (cam_kps,cam_descs) = detector.detectAndCompute( cam_gray, None )
+
+    print('Input image: keypoints = ' + str( len(cam_kps) ) + ' , descriptors = ' + str(cam_descs.shape) )
+
+    # Find the matches for each descriptor in 'template_descs'
+    matches = flann.knnMatch( template_descs,   # query set of descriptors
+                              cam_descs ,       # train set of descriptors
+                              k=2 )             # only find two best matches for each query descriptor
+
+    goodMatches = []
+    for i, mn in enumerate(matches):
+        if ( len(mn) == 2 ):                # prevent the case when only one match is found
+            m = mn[0]  ;   n = mn[1]        # 'm' is the best match, 'n' is the second-best match
+            if m.distance < 0.7 * n.distance: 
+            # if m.distance < 0.6 * n.distance:   
+                goodMatches.append( m )
+
+    MIN_MATCH_COUNT = 50
+    homoResult = cam_bgr.copy()
+    print(len(goodMatches))
+    if len ( goodMatches ) < MIN_MATCH_COUNT:
+        print ("Not enough matches found for computing the homography.")
+        return None,False
+    else:        
+        template_pts = np.float32([ template_kps[m.queryIdx].pt for m in goodMatches ]).reshape(-1,1,2)
+        cam_pts = np.float32([ cam_kps[m.trainIdx].pt for m in goodMatches ]).reshape(-1,1,2)
+
+        # Compute the 3x3 homography matrix for converting points in model image to points in camera image
+        H_m2c, mask = cv2.findHomography(template_pts, cam_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        print(H_m2c)
+        if H_m2c is None:
+            print ("Not enough matches found for computing the homography.")
+            return None,False
+        else:
+            # Find four corners of the template image in camera coordinate
+            h,w = template_gray.shape[:2]
+            src_pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst_pts = cv2.perspectiveTransform(src_pts, H_m2c)
+
+            hmcp = homoResult.copy()
+            homoResult = cv2.polylines( homoResult,             # image  
+                                        [np.int32(dst_pts)],    # array of polygonal curves
+                                        True,                   # draw polylines as closed polygons
+                                        (0,255,0),             # line color
+                                        2, cv2.LINE_AA )         # line thickness , line's type
+            rect = cv2.boundingRect(dst_pts)
+            x, y, w, h = rect
+            croped = homoResult[y:y+h, x:x+w].copy()
+            area = w * h
+            print(x, y, w, h)
+            print('area:', area)
+            if area < 30000 or x > 0 or y > 0:
+                print("image out of bound")
+                return None,False
+            else:
+                pts = np.array(dst_pts.reshape(4,2), dtype = "float32")
+                warped = four_point_transform(hmcp, pts)
+                cv2.imshow('warped', warped)
+                # cv2.imwrite('croped_img.png', warped)
+    return warped,True
+
 
 def detect_x():
     global tmp
@@ -125,7 +233,8 @@ state = 0
 def show_frame():
     global state
     _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+    res,ok = detect_card(frame)
+    # frame = cv2.flip(frame, 1)
     frame =cv2.resize(frame,dim,interpolation = cv2.INTER_AREA)
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     img = Image.fromarray(cv2image)
@@ -141,7 +250,10 @@ def show_frame():
 
     # # Display Score
     if state == 0:
-        if detect_card():
+        
+        print(ok)
+        print(res)
+        if ok:
             party = detect_x()
             countScore(party)
             state = 1
