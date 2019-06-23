bc6ecb278b3b0b69ddd06335f97d030d16f9acbf
Thanawat Polcharoen <taeint93@gmail.com>
2019-06-23 20:15:31 +0700

    specific camera resolution to 720p

--- raspberry_pi_camera_client.py
+++ raspberry_pi_camera_client.py
@@ -10,8 +10,9 @@ import imagezmq
 sender = imagezmq.ImageSender(connect_to='tcp://192.168.137.1:5555')
 
 rpi_name = socket.gethostname()
-picam = VideoStream(usePiCamera=True).start()
+picam = VideoStream(usePiCamera=True, resolution=(1280, 720)).start()
 time.sleep(2.0)  # allow camera sensor to warm up
 while True:
     image = picam.read()
+    print(image.shape)
     sender.send_image(rpi_name, image)
